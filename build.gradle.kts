9a7a49751184159c1b1b2988889dd8639784626d
Daniel A. Freer <danielfreer@gmail.com>
2020-09-20 22:28:29 -0400

    Added kotest for unit testing

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,8 @@ repositories {
 }
 dependencies {
     testImplementation(kotlin("test-junit5"))
+    testImplementation("io.kotest:kotest-runner-junit5:4.2.5")
+    testImplementation("io.kotest:kotest-assertions-core:4.2.5")
 }
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "13"
