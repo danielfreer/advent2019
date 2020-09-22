68f2803ee1cbc6eaa65a8b90009f1940020fd255
Daniel A. Freer <danielfreer@gmail.com>
2020-09-21 23:49:53 -0400

    Day 4 part 2 solved

--- src/main/kotlin/day4.kt
+++ src/main/kotlin/day4.kt
@@ -11,16 +11,24 @@ fun main(vararg args: String) {
     }
     println("Day 4 - Part 1 Solution took: $firstDuration")
     println("Number of possible passwords: $numPossiblePasswords")
+
+    val (numPossiblePasswordsRedux, secondDuration) = measureTimedValue {
+        possiblePasswordsRedux(range).size
+    }
+    println("Day 4 - Part 2 Solution took: $secondDuration")
+    println("Number of possible passwords: $numPossiblePasswordsRedux")
 }
 
 private typealias Rule<T> = (T) -> Boolean
+
+private fun Int.toList() = toString().map(Char::toString).map(String::toInt)
 
 @Suppress("SameParameterValue")
 private fun isLength(length: Int): Rule<Int> = { it.toString().length == length }
 private fun isWithinRange(range: IntRange): Rule<Int> = { it in range }
 private val isTwoAdjacentDigitsTheSame: Rule<Int> = {
     var isTwoAdjacentDigitsTheSame = false
-    it.toString().map(Char::toInt).reduce { acc, i ->
+    it.toList().reduce { acc, i ->
         if (acc == i) isTwoAdjacentDigitsTheSame = true
         i
     }
@@ -28,7 +36,7 @@ private val isTwoAdjacentDigitsTheSame: Rule<Int> = {
 }
 private val isIncreasingOrSame: Rule<Int> = {
     var isIncreasingOrSame = true
-    it.toString().map(Char::toInt).reduce { acc, i ->
+    it.toList().reduce { acc, i ->
         if (i < acc) isIncreasingOrSame = false
         i
     }
@@ -40,6 +48,24 @@ fun possiblePasswords(range: IntRange): Set<Int> {
         .filter(isLength(6))
         .filter(isWithinRange(range))
         .filter(isTwoAdjacentDigitsTheSame)
+        .filter(isIncreasingOrSame)
+        .toSet()
+}
+
+private val hasExactlyTwoAdjacentDigitsTheSame: Rule<Int> = { password ->
+    val adjacentDigitsCounts = mutableMapOf<Int, Int>()
+    password.toList().reduce { acc, i ->
+        if (acc == i) adjacentDigitsCounts.compute(i) { _, value -> value?.inc() ?: 2 }
+        i
+    }
+    adjacentDigitsCounts.values.any { it == 2 }
+}
+
+fun possiblePasswordsRedux(range: IntRange): Set<Int> {
+    return range.asSequence()
+        .filter(isLength(6))
+        .filter(isWithinRange(range))
+        .filter(hasExactlyTwoAdjacentDigitsTheSame)
         .filter(isIncreasingOrSame)
         .toSet()
 }
--- src/test/kotlin/Day4Test.kt
+++ src/test/kotlin/Day4Test.kt
@@ -20,4 +20,21 @@ class Day4Test : DescribeSpec({
                 .shouldNotContain(123789)
         }
     }
+    describe("possiblePasswordsRedux") {
+        it("when range includes 112233, should contain 112233") {
+            val range = 112233..112233
+            possiblePasswordsRedux(range)
+                .shouldContain(112233)
+        }
+        it("when range includes 123444, should not contain 123444") {
+            val range = 123444..123444
+            possiblePasswordsRedux(range)
+                .shouldNotContain(123444)
+        }
+        it("when range includes 111122, should contain 111122") {
+            val range = 111122..111122
+            possiblePasswordsRedux(range)
+                .shouldContain(111122)
+        }
+    }
 })
